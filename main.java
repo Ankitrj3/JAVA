0d4fa2cd7bfa792c00fae06d99622fb330dcc203
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-12-16 11:19:10 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+class main{
+    public void get(){
+       int a =12;
+       System.out.println(a);
+       System.out.println("hello ankit ranjan");
+    }
+public static void main(String [] args){
+      main k = new main();
+      k.get();
+}
+}
