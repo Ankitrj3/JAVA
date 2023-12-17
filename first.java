af69fe215bcf02ce4de468ea5edbe7a38392f172
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-12-17 09:44:56 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,35 @@
+class first{
+    public static void main(String args[]){
+        System.out.println("hello mr ankit ");
+        System.out.println("hello world");
+        System.out.println("hello world with java");
+
+      
+
+        System.out.println("*\n**\n***\n****\n");
+        String name ="ankit ranjan";
+        int age =12;
+        float sal = 3009/4;
+        boolean a = true;
+        System.out.println(age);
+        float c=age+sal;
+        double k= 1323.23;
+        System.out.println(c);
+        System.out.println(k);
+        System.out.println(name);
+
+
+        //multiply
+        int af = 213;
+        int b =34;
+        int ck = af*b;
+        System.out.println(ck);
+
+        //in java bodmass rule don't work
+        int l = 10;
+        int m = 5;
+        int ans = (l*m)/(l-m);
+        System.out.println(ans);
+
+    }
+}
