cadba7f458f744e17eaf73d0d77880bdeb44dea4
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-12-27 19:17:44 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+public class switch1{
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("sunday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            case 3:
+                System.out.println("tuesday");
+                break;
+            case 4:
+                System.out.println("weddnesday");
+                break;
+            case 5:
+                System.out.println("thrusday");
+                break;
+            case 6:
+            System.out.println("friday");
+            break;
+            case 7:
+                System.out.println("saturday");
+                break;
+            default:
+                System.out.println("invalid");
+                break;
+        }
+    }
+}
