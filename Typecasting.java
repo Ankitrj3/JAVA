b56eb82837c7fbefaa45525b65aae62f1582a922
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-12-29 14:20:08 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,16 @@
+public class Typecasting {
+    public static void main(String args []){
+        int a = 234;
+        System.out.println(a);
+
+        double i = (double)a; //typecasting
+        System.out.println(i);
+
+        double k = 34.56;
+        System.out.println(k);
+        
+        int j = (int)k;
+        System.out.println(j);
+
+    }
+}
