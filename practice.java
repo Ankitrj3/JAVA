b46e3967a91d7b7ff71254ae044e618cbbec5164
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-12-29 12:07:12 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class practice {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=0;
+        do{
+           System.out.println("hello hi");
+           i++;
+        }while(i<n);
+    }
+}
