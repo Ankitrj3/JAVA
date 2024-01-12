b276691307b2542efc47c16c9e89c51dc182485e
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-01-12 16:16:47 +0530

    Add files via upload

---
+++ Or.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Or {
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       int a = Integer.parseInt(sc.nextLine(),2);
+       int b = Integer.parseInt(sc.nextLine(),2);
+
+       int resultor = a | b;
+       System.out.println(resultor);
+    }
+}
