fc5d1f33257a92f099cbb7ef8524e50458851cbb
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-01-16 12:55:48 +0530

    Add files via upload

---
+++ Execption.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.ExecutionException;
+
+public class Execption {
+    public static void main(String[] args) {
+        int i = 0;
+        int j =0;
+        try
+        {
+         j = 18/i;
+        }catch(Exception e){
+            System.out.println("Something went wrong");
+        }
+        System.out.println(j);
+        System.out.println("bye ");
+    }
+}
