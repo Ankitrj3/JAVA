80145fe32223d48d2d15b17721735accfc2263ca
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-01-17 09:05:52 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Foreach {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+
+        int arr [] ={12,34,56,3,89};
+
+        for(int k : arr){
+            System.out.println(k + " array elements");
+        }
+    }
+}
