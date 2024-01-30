d58f8ae303a9c7b4381648604c422240f7c5e2b5
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-01-30 11:34:09 +0530

    basic linked list code

---
+++ LinkedList.java
@@ -0,0 +1,57 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+   
+    public void append(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = newNode;
+    }
+
+    // Print the elements of the linked list
+    public void printList() {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(3);
+
+        System.out.println("Linked List: ");
+        linkedList.printList();
+    }
+}
+
