c86dde5d4bffe52d38377fa994fc1873fc8ed7e6
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-02-05 15:23:03 +0530

    Add files via upload

---
+++ Stringfun.java
@@ -0,0 +1,14 @@
+public class Stringfun {
+    public static void main(String[] args) {
+        String str = "ANKIT RANJAN";
+        System.out.println(str.length());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.charAt(4));
+        System.out.println(str.indexOf("R"));
+        System.out.println(str.lastIndexOf("A"));
+        System.out.println(str.concat("robin"));
+        System.out.println(str.equals("ANKIT RANJAN"));
+        System.out.println(str.equalsIgnoreCase("ANKIT RANJAN"));
+    }
+}
