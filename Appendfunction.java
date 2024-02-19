4cd4c127700649e8c59ad182f6982633b6d32f45
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-02-19 16:46:50 +0530

    Add files via upload

---
+++ Appendfunction.java
@@ -0,0 +1,11 @@
+public class Appendfunction {
+    public static void main(String args[]){
+        String strv = args[0];
+
+        java.lang.StringBuilder ank = new java.lang.StringBuilder();
+        ank.append(strv);
+        ank.append("rj");
+
+        System.out.println(ank);
+    }
+}
