c6f57dcc5635716215b0989dc3acb10114062cf7
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-02-19 10:11:21 +0530

    Add files via upload
    input - ankit
    a
    an
    ank
    anki
    ankit

---
+++ SubStringStair.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SubStringStair {
+    public static void main(String args[]){
+        String str = args[0];
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.substring(0,i));
+        }
+    }
+}
