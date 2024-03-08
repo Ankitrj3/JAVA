f37d4979fb00eb5427c1be1d7d61f959947fc53f
Ankitrj3 <ankitrobinranjan@gmail.com>
2024-03-08 17:21:21 +0530

    updating java repo

---
+++ Pracpalindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Pracpalindrome {
+   
+        public static void main(String args[]){
+             Scanner sc = new Scanner(System.in);
+             String a = sc.nextLine();
+    
+             java.lang.StringBuilder build = new java.lang.StringBuilder();
+             build.append(a);
+             build.reverse();
+    
+             if(build.toString().equals(a)){
+                System.out.println("plaindrome");
+             }else{
+                System.out.println("not plaindrome");
+             }
+        }
+    }
+       
+
