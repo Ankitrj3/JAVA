c63a677e56461c5e3e948d151117f1d71a639e2c
Ankitrj3 <ankitrobinranjan@gmail.com>
2024-03-19 11:48:43 +0530

    mutiliple inheritance

---
+++ inheritance.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class first {
+    public int a;
+    public int b;
+
+    void add() {
+        Scanner la = new Scanner(System.in);
+        a = la.nextInt();
+        b = la.nextInt();
+        System.out.println(a + b);
+    }
+}
+
+class second extends first {
+    void sub() {
+        Scanner la = new Scanner(System.in);
+        System.out.println(a - b);
+    }
+}
+
+class third extends first {
+    void mul() {
+        System.out.println(a * b);
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        third th = new third();
+        th.add(); 
+        th.mul();
+    }
+}
